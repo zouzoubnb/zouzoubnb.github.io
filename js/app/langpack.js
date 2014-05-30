7bb36906d96f05c070c45ea5c5f2553baeee4303
Andre Lee <andrelee@qnap.com>
2014-05-30 14:54:07 +0800

    i18n

---
+++ js/app/langpack.js
@@ -0,0 +1,11 @@
+var LANGPACK = {
+    "en": {
+        'Home': 'Home',
+
+    },
+
+    "zh_TW": {
+        'Home': '首頁',
+
+    }
+}
--- js/app/main.js
+++ js/app/main.js
@@ -1,8 +1,33 @@
 var app = angular.module('myApp', []);
 
 app.controller('MainCtrl', function ($rootScope) {
+    var getCurrentLang, getQueryParam;
+
+    getQueryParam = function(name) {
+      var currentQueryParams;
+      currentQueryParams = new URI().query(true);
+      return currentQueryParams[name];
+    };
+
+    window.getQueryParam = getQueryParam;
+
+    getCurrentLang = function() {
+      var lang;
+      lang = getQueryParam("lang");
+      if (lang) {
+        return lang.replace("-", "_");
+      } else {
+        return void 0;
+      }
+    };
+
+    window.getCurrentLang = getCurrentLang;
+
+
     $rootScope._ = function(key){
         console.log('key', key);
+        console.log('getCurrentLang', getCurrentLang());
+
     };
 
 
