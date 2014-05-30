e5317bc0190217959423e94a1616c95a21e9d8ec
Andre Lee <andrelee@qnap.com>
2014-05-30 15:56:16 +0800

    i18n

--- js/app/langpack.js
+++ js/app/langpack.js
@@ -13,7 +13,13 @@ var LANGPACK = {
         'Your Name': '您的姓名',
         'Your Email': '您的電子郵件',
         'Your Phone': '您的聯絡電話',
-        'Send Message': '確定送出訊息'
+        'Send Message': '確定送出訊息',
+        'Delicious Food': '美食',
+        'Together': '相聚',
+        'Moment': '美好時光',
+        'Heart': '真心款待',
+        'Reasons to choose ZouZou B&B?': '為什麼要選擇走走?',
+        'Our Rooms': '精緻房型',
 
     }
 }
