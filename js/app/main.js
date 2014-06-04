0915e95c35b1ebd8988d105c0b24c2ad4590411c
Andre Lee <andrelee@qnap.com>
2014-06-04 11:38:35 +0800

    added lang detection

--- js/app/main.js
+++ js/app/main.js
@@ -15,13 +15,27 @@ function MainCtrl($rootScope) {
       if (lang) {
         return lang.replace("-", "_");
       } else {
-        return void 0;
+        var lang_browser = getCurrentLangByBrowserSettings();
+        console.log('browser lang:', lang_browser);
+        switch(lang_browser){
+            case 'zh':
+            case 'zh_TW':
+                return 'zh_TW';
+                break;
+            default:
+                return 'en';
+        }
       }
     };
 
+    getCurrentLangByBrowserSettings = function(){
+        var lang = window.navigator.userLanguage || window.navigator.language;
+        return lang;
+    }
+
     window.getCurrentLang = getCurrentLang;
 
-    $rootScope.lang = getCurrentLang() ? getCurrentLang() : 'en';
+    $rootScope.lang = getCurrentLang();
 
     $rootScope._ = function(key){
         var lang = getCurrentLang();
