f4944bb8c67baf7c9039a149b3e3d7d11e00dbd6
Andre Lee <andrelee@qnap.com>
2014-07-25 19:10:35 +0800

    blog integrated.

--- js/app/main.js
+++ js/app/main.js
@@ -1,4 +1,7 @@
 function MainCtrl($rootScope) {
+    var BLOG_API_URL = "http://api.tumblr.com/v2/blog/zouzoubnb.tumblr.com/posts";
+    var BLOG_API_KEY = "YT6dJznWbZF0VtgHTNRZD5BRHUkM3VkC7GkXfIvpt1WtPuyra8";
+    var BLOG_TAG = "ZOUZOU_HOME";
     var getCurrentLang, getQueryParam;
 
     getQueryParam = function(name) {
@@ -23,7 +26,9 @@ function MainCtrl($rootScope) {
                 return 'zh_TW';
                 break;
             default:
+                console.log('default', lang_browser);
                 return 'en';
+                break;
         }
       }
     };
@@ -55,4 +60,48 @@ function MainCtrl($rootScope) {
             return key;
         }
     };
+
+    $rootScope.blog_posts = [];
+
+    getBlogRss = function(){
+        $.ajax({
+             url: BLOG_API_URL,
+             method: 'get',
+             data : ({
+                 api_key: BLOG_API_KEY,
+                 tag: BLOG_TAG,
+                 jsonp: 'processBlogData'
+             }),
+             dataType: "jsonp"
+         });
+    }
+
+    processTags = function(tags){
+        var tags_str = "";
+        for(var i=0; i<tags.length; i++){
+            if(tags[i]!==BLOG_TAG){
+                tags_str = tags_str + ", " + BLOG_TAG;
+            }
+        }
+
+        if (tags_str.slice(0,2) === ", "){
+            tags_str = tags_str.slice(2);
+        }
+        return tags_str;
+    }
+
+    processBlogData = function(data) {
+        console.log(data.response);
+        if (data.response && data.response.posts){
+            posts = data.response.posts;
+            for(var i=0; i<posts.length; i++){
+                var item = {title: posts[i].title, date: posts[i].date,  post_url: posts[i].post_url, tags: processTags(posts[i].tags)};
+                $rootScope.blog_posts.push(item);
+            }
+        }
+        $rootScope.$apply(); 
+    }
+
+    getBlogRss();
+
 }
